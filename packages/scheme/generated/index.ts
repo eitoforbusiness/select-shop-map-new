091b5cd5fb3a180ce724b834dab731ea3af12206
Murataforbusiness <murataforbusiness@gmail.com>
2025-05-24 03:45:56 +0900

    修正: api送信できるようになった

---
+++ packages/scheme/generated/index.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { Shop } from './models/Shop';
+export type { ShopInput } from './models/ShopInput';
+
+export { DefaultService } from './services/DefaultService';
