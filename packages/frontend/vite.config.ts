16a643645dbe707feb25af046a7b17b3ca96861d
Murataforbusiness <murataforbusiness@gmail.com>
2025-05-24 02:04:15 +0900

    修正

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    strictPort: false,
+    port: 3000,
+    strictPort: true,
     host: '0.0.0.0',
     open: true
   }
